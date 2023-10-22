9d9a89b9b19ebdb985cd80cb18b1dc63eea305e6
Justin Jr. Minaya <justinjrminaya@gmail.com>
2023-10-22 15:32:46 -0400

    adding fallback

--- src/helpers/fetchRandomUsers.js
+++ src/helpers/fetchRandomUsers.js
@@ -1,9 +1,16 @@
+ import cardsMock from "../mocks/cards.json";
+
 export const fetchRandomUsers = async () => {
-    const response = await fetch("https://randomuser.me/api?results=4");
-    let { results } = await response.json();
-    results = results.map(x => ({
-        key: x.name.first,
-        src: x.picture.large,
-    }));
-    return results;
+    try {
+        const response = await fetch("https://randomuser.me/api?results=4");
+        let { results } = await response.json();
+        results = results.map(x => ({
+            key: x.name.first,
+            src: x.picture.large,
+        }));
+        return results;
+    } catch (err) {
+        console.log(err);
+        return cardsMock;
+    }
 }
